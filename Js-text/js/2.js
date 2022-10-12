58bb937bf057d71a6ccc4b01a8d267d1752fee4a
lxhroot <lxh94088@petalmail.com>
2022-10-12 23:45:39 +0800

    js&&css&&node

---
+++ Js-text/js/2.js
@@ -0,0 +1,13 @@
+function big(){
+	document.body.style.fontSize = "25px"
+}
+function center(){
+	document.body.style.fontSize = "16px"
+}
+function samll(){
+	document.body.style.fontSize = "5px"
+}
+
+// function size1(size){
+// 	document.body.style.fontSize = size
+// }
